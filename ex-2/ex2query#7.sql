21a6095ec14b39ef414ef792e98f265219b08a43
jessymatta <jessy.w.matta@gmail.com>
2022-09-11 17:50:05 +0300

    fixed query#7 of ex2

--- ex-2/ex2query#7.sql
+++ ex-2/ex2query#7.sql
@@ -1,18 +1,25 @@
 -- Find the number of CS students enrolled in any course
 --Note that I understood the question in two ways, so I implemented them both
-
-
 --First option: find the number of CS students enrolled in each course
 SELECT
-    course.name,
+    course.crn,
     COUNT(enrolled.student_id)
 FROM
-    course
-    LEFT JOIN enrolled ON enrolled.course_crn = course.crn
+    student,
+    course,
+    enrolled,
+    department,
+    majorsin
+WHERE
+    enrolled.student_id = student.id
+    AND enrolled.course_crn = course.crn
+    AND majorsin.student_id = student.id
+    AND majorsin.dept_id = department.id
+    AND department.name = "CS"
 GROUP BY
     course.crn;
 
---Second option: find the total number of enrolled students (students who are enrolled in at least one course)
+--Second option: find the total number of enrolled CS students (students who are enrolled in at least one course)
 SELECT
     COUNT(student.name)
 FROM
