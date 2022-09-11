0967b4883631cfa18edd7303410d96a2f1da9bd0
jessymatta <jessy.w.matta@gmail.com>
2022-09-11 17:04:20 +0300

    added query#4 for ex2

---
+++ ex-2/ex2query#4.sql
@@ -0,0 +1,15 @@
+SELECT
+    student.name,
+    course.crn
+FROM
+    student,
+    department,
+    majorsin,
+    course,
+    enrolled
+WHERE
+    majorsin.student_id = student.id
+    AND majorsin.dept_id = department.id
+    AND department.name = 'BIF'
+    AND enrolled.student_id = student.id
+    AND enrolled.course_crn = course.crn;
